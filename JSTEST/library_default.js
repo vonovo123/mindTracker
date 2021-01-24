184e26410ff2477cd845debcba802e3cd8633362
dynamic-henry-kwon <jukdo67@gmail.com>
2021-01-24 10:39:21 +0900

    20210124

--- JSTEST/library_default.js
+++
@@ -1,3 +0,0 @@
-const fncPlusNumbers = (pNum1, pNum2) => pNum1 + pNum2
-
-export default fncPlusNumbers
--- JSTEST/library_named.js
+++
@@ -1,3 +0,0 @@
-const conhello = '안녕하세요'
-const fncPlusNumbers = (pNum1, pNum2) => pNum1 + pNum2
-export { conhello, fncPlusNumbers }
